24a1d62f207912839100108b2bd2cf9a2122ed4c
Aliaksei Bialiauski <Hialexbel@gmail.com>
2023-02-14 17:09:17 +0300

    #1

---
+++ src/main/java/com/h1alexbel/weeks/WeeksApplication.java
@@ -0,0 +1,12 @@
+package com.h1alexbel.weeks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeksApplication {
+
+  public static void main(final String... args) {
+    SpringApplication.run(WeeksApplication.class, args);
+  }
+}
