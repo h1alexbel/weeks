7fe357ea9641cfcbd91bea65ddd084aee848cd6c
Aliaksei Bialiauski <Hialexbel@gmail.com>
2023-03-07 14:59:01 +0300

    test for codecov run

---
+++ src/test/java/com/h1alexbel/weeks/WeeksApplicationTests.java
@@ -0,0 +1,12 @@
+package com.h1alexbel.weeks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeksApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
