b83c75d538e93a40e86b0033cbadb77d19564432
Aliaksei Bialiauski <Hialexbel@gmail.com>
2023-03-07 15:02:43 +0300

    VwWeek test

--- src/test/java/com/h1alexbel/weeks/WeeksApplicationTests.java
+++ src/test/java/com/h1alexbel/weeks/WeeksApplicationTests.java
@@ -1,5 +1,7 @@
 package com.h1alexbel.weeks;
 
+import com.h1alexbel.weeks.route.VwWeek;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,5 +10,8 @@ class WeeksApplicationTests {
 
   @Test
   void contextLoads() {
+    final String title = "test";
+    final VwWeek view = new VwWeek(title, "", "");
+    Assertions.assertThat(view.getTitle()).isEqualTo(title);
   }
 }
